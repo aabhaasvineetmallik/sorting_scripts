7e074c3dbcd7639580a5670ac04774dcafa5fb86
shringi <ankurshringi@iisc.ac.in>
2020-04-23 20:45:38 +0530

    Added multiple major features
    1. Added optional parameters
    -n for number of words to be searched
    -p for number of first pdf pages to be searched
    -o for choosing the "And" or "OR" operator when multiple keywords are given.
    2.  less command in given problems in windows 10. So replaced it with another optimized python library pdftotext and after that it usase the less command. One advantage of using this library is that we can directory mention how many pages to be searched instead of converting entire pdf into text. It should boost the performance of the script as we usually search first few pages only.
    3.  Reflowed the for loop now to accommodate the "AND" operator usage. This decrease the speed of the code but improves the functionality.
    4. Output now goes to a dedicated folder called summary where all the pdf goes. Needs to be optional in future version.
    5. Improved the user instructions to be displayed at command prompt.

--- sorting_script_html.sh
+++ sorting_script_html.sh
@@ -20,16 +20,90 @@
 # Put following command to get further instructions on how to use it
 # ./sorting_script_html.sh <keyword>
 
-N_MAX=100 	# Number of lines to be read from the starting of the file
+Operator="|"
+N_max="1000"
+page_f=2
+# Section with optional arguments------------------------------------------------------------------
+helpFunction()
+{
+   echo ""
+   echo "  Usage: $0 -o 'Operator' -p 'page_f' -n 'N_max' keyword_1 keyword_2"
+   echo
+   echo -e "\t-o Operator: string ('&' or '|') ; default is '|' "
+   echo -e "\t           : Operator to be applied if multiple keywords are entered!"
+   echo
+   echo -e "\t-p page_f  : number ( >=01 )     ; default is 2"
+   echo -e "\t           : Page number till where pdf needs to be searched"
+   echo
+   echo -e "\t-n N_max   : number ( >=50)      ; default is 1000"
+   echo -e "\t           : Maximum number of words in which search needs to be made!"
+   echo
+   echo -e 
+   echo -e "Valid Examples- "
+   echo -e "\t $0 facebook "
+   echo -e "\t $0 facebook experience"
+   echo -e "\t $0 -p 2 -n 100 facebook experience"
+   echo -e "\t $0 -p 2 -n 1000 -o '&' facebook experience"
+   echo
+   echo -e "Invalid Examples"
+   echo -e "\t $0 facebook experience -p 2 -n 100 "
+   
+   exit 1 # Exit script after printing help
+}
+OPTIND=1
+while getopts ":o:p:n:" opt
+do
+   case "$opt" in
+      n ) N_max="$OPTARG" ;;	  
+      o ) Operator="$OPTARG" ;;
+      p ) page_f="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
 
-file="$@.html"
+# Print helpFunction in case parameters are empty
+# if [ -z "$N_max" ] || [ -z "$page_f" ]
+# then
+   # echo "Some or all of the parameters are empty";
+   # helpFunction
+# fi
+echo
+echo "                Operator for search strings : $Operator"
+echo "             Number of pages being searched : $page_f"
+echo "In these pages, no. of words being searched : $N_max"
+# Begin script in case all parameters are correct
+shift "$((OPTIND-1))"
+
+# Creating html file in a folder in parent directory------------------------------------------------------------------
+pd="$(dirname "${PWD}")" # Parent Directory
+#echo "$pd"
+folder_html="Summary" 
+path_folder_html="$pd/$folder_html"
+#echo "$path_folder_html"
+mkdir -p "$path_folder_html"
+# Sorting multiple arguments.
+if [ "$Operator" == "&" ]
+then
+	join="_x_"
+else
+	join="_+_"
+fi
+name=$(for var in "$@"; do
+    echo "$var"
+done | sort | paste -s -d '#' | sed "s/\#/$join/g")
+file="$path_folder_html/$name.html"
+rm "$file" 2> /dev/null
 #echo "$file"
+#echo "$file"
+N_max="$N_max" 	# Number of lines to be read from the starting of the file
+page_i=1
+page_f=2
 folder="${PWD##*/}"
-rm "$file"
+
 echo "<!DOCTYPE html>" >> "$file"
 echo "<html>" >> "$file"
 echo "<head>" >> "$file"
-echo "<title>"$@"</title>" >> "$file"
+echo "<title>"$name"</title>" >> "$file"
 echo "<meta name='viewport' content='width=device-width, initial-scale=1'/>" >> "$file"
 echo '<script type="text/javascript">' >> "$file"
 cat "toc.js" >> "$file"
@@ -39,22 +113,39 @@ echo '<body onload="generateTOC(document.getElementById(`toc`));">' >> "$file"
 echo '<div id="toc"></div>' >>"$file"
 if [ $# -eq 0 ]
 then
-	echo "enter keywords separated by a space"
+	echo
+	echo "Error: Missing a keyword to be searched!!"
+	echo
+	echo "For more details type following:"
+	echo -e "$0 -?"	
+	echo
 	echo "if more than one keyword is given, all matches will be displayed"
 	echo 'If you want to search for a phrase put it in quotes Ex. "Quantum Computing"'
 	echo "a file will always be created giving the context in which the keywords appear in the pdf files"
 elif [ $# -eq 1 ]
 then
-	echo "The number of inputs keywords is: 1"
-	echo "A case insensitive search will be carried out in first '"$N_MAX"' words of the pdfs."
-	echo "  (Change the number of words to search by changing 'N_MAX' parameter in the script)"
-	echo "Results are being segregated in the file: '"$file"'"
+	echo
+	echo "    The number of inputs search keywords is : 1"
+	echo
+	echo "   Results are being segregated in the file : '"$file"'"
+	echo
+	echo "Use '-n' option to change the number of words from which to search "
+	echo
+	echo "For more details type following:"
+	echo -e "$0 -?"
+	
 else
-	echo "The number of inputs keywords is: "$# ""
-	echo "A case insensitive search will be carried out in first '"$N_MAX"' words of the pdfs."
-	echo "  (Change the number of words to search by changing 'N_MAX' parameter in the script)"
-	echo "Results are being segregated in the file: '"$file"';"
+	echo
+	echo "    The number of inputs search keywords is : "$# ""
+	echo
+	echo "   Results are being segregated in the file : '"$file"'"
+	echo "Use '-n' option to change the number of words from which to search "
+	echo "Use '-o' option to give operator '&' or '|' to use as boolean operator."
+	echo
+	echo "For more details type following:"
+	echo -e "$0 -?"	
 	echo "  (Results from first keyword will appear first, followed by the rest. Scroll down to see other keywords.)"
+	echo
 fi
 echo
 # progress bar function
@@ -76,30 +167,51 @@ then
 		prog "$(($(($f*100))/$count))" "Progress" # Displaying percentage progress (number of pdfs scanned)
 		f=$(($f+1))
 		FLAG=0
-		for ((j=1; j<$#+1; j=j+1))
-		do
-			less $i | head -$N_MAX > ".$file.tmp.txt"
-			COMMAND_ONE=$(grep -in ${!j} -C 5 ".$file.tmp.txt" | tee ".$file.text.txt")		#${!#} gives access to the last argument supplied to the script
-			COMMAND_TWO=$(pdfinfo $i 2>/dev/null | grep -i ${!j})
-			if [ -n "$COMMAND_ONE" ] || [ -n "$COMMAND_TWO" ]	#'-n' checks that the string is not empty. for more options see 'man test'
-			then
-				FLAG=1
-				echo '<h2 id = "'$i'">' >> "$file"
-				echo -e '<a href = "../'$folder'/'$i '" target = "_blank">' $i'</a></h2>' >> "$file"
-				echo '<a href="#toc">Go back to the table of contents!</a>' >> "$file"
-				echo '<pre>' >> "$file"
-				echo '<br>KEYWORD '$j': '${!j} >> "$file"		# -e enables the interpretation of '\' characters
-				sed -i -e ':a' -e 'N' -e '$!ba' -e 's/\n/<br>/gI' ".$file.text.txt" #Replacing new line character with <br>
-				sed -i -e "s/${!j}/<mark>${!j}<\\/mark>/gI" ".$file.text.txt" #Highlighting the keyword wherever it is appearing
-				cat ".$file.text.txt" >> "$file"
-				echo "</pre>" >> "$file"
-				continue
-			else
-				FLAG=0
-				j=$(($#+1))
-			fi
-		done
-
+		pdftotext -f $page_i -l $page_f $i -| less -f | head -$N_max > ".temp.tmp.txt"
+		
+		if [ "$Operator" == "&" ]
+		then
+			stop=0			
+			for ((k=1; k<$#+1; k=k+1))
+			do
+				COMMAND=$(grep -in ${!k} -C 5 ".temp.tmp.txt")
+				if [ -n "$COMMAND" ]
+				then
+					stop=$(($stop+1))
+						continue
+				else
+					break
+				fi
+				echo "&:$stop"
+			done
+		else
+			stop=$(($#))
+		fi	
+		if [ "$stop" -eq $# ]
+		then	
+			for ((j=1; j<$#+1; j=j+1))
+			do			
+				COMMAND_ONE=$(grep -in ${!j} -C 5 ".temp.tmp.txt" | tee ".temp.text.txt")		#${!#} gives access to the last argument supplied to the script
+				COMMAND_TWO=$(pdfinfo $i 2>/dev/null | grep -i ${!j})
+				if [ -n "$COMMAND_ONE" ] || [ -n "$COMMAND_TWO" ]	#'-n' checks that the string is not empty. for more options see 'man test'
+				then
+					FLAG=1
+					echo '<h2 id = "'$i'">' >> "$file"
+					echo -e '<a href = "../'$folder'/'$i '" target = "_blank">' $i'</a></h2>' >> "$file"
+					echo '<a href="#toc">Go back to the table of contents!</a>' >> "$file"
+					echo '<pre>' >> "$file"
+					echo '<br>KEYWORD '$j': '${!j} >> "$file"		# -e enables the interpretation of '\' characters
+					sed -i -e ':a' -e 'N' -e '$!ba' -e 's/\n/<br>/gI' ".temp.text.txt" #Replacing new line character with <br>
+					sed -i -e "s/${!j}/<mark>${!j}<\\/mark>/gI" ".temp.text.txt" #Highlighting the keyword wherever it is appearing
+					cat ".temp.text.txt" >> "$file"
+					echo "</pre>" >> "$file"
+					continue
+				else
+					FLAG=0
+					j=$(($#+1))
+				fi
+			done
+		fi
 		if [ $FLAG -eq 0 ]
 		then
 			continue
@@ -107,7 +219,8 @@ then
 	done
 	IFS="$OIFS"
 fi
-rm ".$file.text.txt" ".$file.tmp.txt"
+rm ".temp.text.txt" ".temp.tmp.txt"
 echo "</body>" >> "$file"
 echo "</html>" >> "$file"
 echo #Empty echo for new line
+echo
